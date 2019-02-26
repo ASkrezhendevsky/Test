914e4009d05e9509ab3db8b55cf9ba882c681fc1
Skrezhendevsky <alekskrzh@gmail.com>
2019-02-26 21:31:26 +0300

    Test commit

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void main(void) {
+	float x, y, z, s;
+	printf("Enter x: ");
+	
+	scanf("%f",&x);
+	printf("Enter y: ");
+	scanf("%f",&y);
+	printf("Enter z: ");
+	scanf("%f",&z);
+	
+	printf("x = %f\n",x);
+	printf("y = %f\n",y);
+	printf("z = %f\n",z);
+	s = log(pow(y,-pow(fabs(x),0.5)))*(x - y/2.0) + sin(atan(z))*sin(atan(z));	
+	
+	printf("\tResult: %f.",s);
+	
+}
